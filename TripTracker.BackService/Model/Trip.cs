3e79a114798369c4d2a297b069d99a7af9b7a9df
Elliot Derhay <e.j.derhay@gmail.com>
2019-02-13 13:47:06 -0500

    Add ID property to Trip class

--- TripTracker.BackService/Model/Trip.cs
+++ TripTracker.BackService/Model/Trip.cs
@@ -7,6 +7,8 @@ namespace TripTracker.BackService.Models
 {
     public class Trip
     {
+        public int Id { get; set; }
+
         public string Name { get; set; }
 
         public DateTime StartDate { get; set; }

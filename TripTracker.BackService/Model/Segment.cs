fa0be5a5eed3377823840e529e563b4ee312a600
Elliot Derhay <e.j.derhay@gmail.com>
2019-02-13 13:50:09 -0500

    Begin building Segment class
    Continue watching video at 20:11
    
    URL for reference: https://www.youtube.com/watch?v=--lYHxrsLsc

---
+++ TripTracker.BackService/Model/Segment.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TripTracker.BackService.Models
+{
+    public class Segment
+    {
+        public int Id { get; set; }
+    }
+}

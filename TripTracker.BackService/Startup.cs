bd1617372edb2bb5235c35f3e15043c1d3d858ee
Elliot Derhay <e.j.derhay@gmail.com>
2019-02-21 15:21:07 -0500

    Add Swagger for generating docs
    Swagger will generate documentation for web APIs.
    
    Continue watching from 49:00

--- TripTracker.BackService/Startup.cs
+++ TripTracker.BackService/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace TripTracker.BackService
 {
@@ -25,11 +26,21 @@ namespace TripTracker.BackService
         {
             services.AddTransient<Models.Repository>();
             services.AddMvc();
+
+            services.AddSwaggerGen(options =>
+                options.SwaggerDoc("v1", new Info{ Title = "Trip Tracker", Version = "v1" })
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options => 
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Trip Tracker v1")
+            );
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
